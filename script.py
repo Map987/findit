23c9ca138cb55d7fe86562564c8c5df135b53600
Map987 <99967856+Map987@users.noreply.github.com>
2024-07-04 02:47:51 +0800

    Update script.py

--- script.py
+++ script.py
@@ -9,11 +9,9 @@ urllib3.disable_warnings()
 # 基础URL和输出文件
 base_url = 'https://esercenti.luckyred.it/film/'
 output_file = 'valid_urls.txt'  # 输出文件名
-max_length = 20  # 设置最大组合长度在 上一层 max_length有效的继续下一层查找，要不然会指数增加耗时
-max_execution_time = 5 * 3600  # 设置最大执行时间为5分钟（例如）
-
-# 用户指定的开始点
-start_point = 'b---'
+start_point_config = 'start_point.config'  # 配置文件名
+max_length = 20  # 设置最大组合长度
+max_execution_time = 5 * 3600  # 设置最大执行时间为5小时
 
 def generate_combinations(prefix, length, valid_prefixes):
     if not valid_prefixes:
@@ -30,13 +28,18 @@ def check_url(url):
     response = requests.get(url, verify=False)
     return response.status_code == 200
 
-def write_valid_url(url):
+def write_valid_url(url, last_valid_url):
     with open(output_file, 'a') as file:
         file.write(url + '\n')
     print(f"Found valid URL: {url}")
+    return url
 
 def timeout_handler(signum, frame):
     raise Exception("Execution time limit reached")
+
+# 读取start_point.config文件获取start_point
+with open(start_point_config, 'r') as file:
+    start_point = file.read().strip()
 
 # 设置定时器
 signal.signal(signal.SIGALRM, timeout_handler)
@@ -52,7 +55,7 @@ try:
         for combination in generate_combinations('', length, valid_prefixes):
             url = f"{base_url}{combination}"
             if check_url(url):
-                write_valid_url(url)
+                last_valid_url = write_valid_url(url, last_valid_url)
                 new_valid_prefixes.append(combination)
         valid_prefixes = new_valid_prefixes
 except Exception as e:
@@ -60,3 +63,7 @@ except Exception as e:
 finally:
     # 取消定时器
     signal.alarm(0)
+
+    # 更新start_point.config文件
+    with open(start_point_config, 'w') as file:
+        file.write(last_valid_url.rsplit('/', 1)[-1] + '\n')
